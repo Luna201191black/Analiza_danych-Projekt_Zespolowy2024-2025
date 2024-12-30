7c3e97ee178dd0c2b9b0f661ec24a9a56aaa4b6c
Michal Owczarek <michaelooo1104@gmail.com>
2024-12-30 11:53:40 +0100

    Kolejne etapy wizualizacji

--- WizualizacjaDanych.R
+++ WizualizacjaDanych.R
@@ -61,42 +61,72 @@ cat("Rozkłady zmiennych liczbowych...\n")
 
 # Wnioskowana kwota - histogram.
 ggplot(data, aes(x = wnioskowana_kwota)) +
-  geom_histogram(bins = 100, fill = "green", alpha = 0.7, color = "black") +
+  geom_histogram(bins = 30, fill = "green", alpha = 0.7, color = "black") +
                    labs(title = "Rozkład wnioskowanej kwoty", x = "Wnioskowana kwota", y = "Liczba wniosków") +
                    theme_minimal() +
-  scale_x_continuous(labels = scales::comma)
+  scale_x_continuous(labels = scales::comma, limits = c(0, 3000000), breaks = seq(0, 3000000, by = 500000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 3000), breaks = seq(0, 3000, by = 500))
+
+# Interpretacja:
+# Większość wniosków dotyczy niewielkich kwot, co jest zauważalne po wysokim słupku w przedziale poniżej 500 000,
+# gdy w tym samym czasie liczba wniosków znacząco maleje wraz ze wzrostem wnioskowanej kwoty.
+# Rozkład jest prawostronnie skośny, przy czym sporadyczne wnioski dotyczą dużych kwot powyżej 2 000 000.
 
 
 # Kwota kredytu - histogram.
 ggplot(data, aes(x = kwota_kredytu)) +
-  geom_histogram(bins = 100, fill = "red", alpha = 0.7, color = "black") +
+  geom_histogram(bins = 30, fill = "red", alpha = 0.7, color = "black") +
   labs(title = "Rozkład kwoty kredytu", x = "Kwota kredytu", y = "Liczba wniosków") +
   theme_minimal() +
-  scale_x_continuous(labels = scales::comma)
+  scale_x_continuous(labels = scales::comma, limits = c(0, 3000000), breaks = seq(0, 3000000, by = 500000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 3000), breaks = seq(0, 3000, by = 500))
+
+# Interpretacja:
+# Zdecydowana większość wniosków dotyczy niskich kwot, co jest zauważalne po wysokim słupku w przedziale poniżej 500 000,
+# gdy w tym samym czasie liczba wniosków znacząco maleje wraz ze wzrostem kwoty kredytu.
+# Rozkład jest prawostronnie asymetryczny, przy czym sporadyczne wnioski dotyczą dużych kwot powyżej 2 000 000.
+
 
 
 # Wkład własny - histogram.
 ggplot(data, aes(x = wklad_wlasny)) +
-  geom_histogram(bins = 100, fill = "blue", alpha = 0.7, color = "black") +
+  geom_histogram(bins = 30, fill = "blue", alpha = 0.7, color = "black") +
   labs(title = "Rozkład wkładu własnego", x = "Wkład własny", y = "Liczba wniosków") +
   theme_minimal() +
-  scale_x_continuous(labels = scales::comma)
+  scale_x_continuous(labels = scales::comma, limits = c(0, 100000), breaks = seq(0, 100000, by = 10000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 2500), breaks = seq(0, 2500, by = 500))
+
+# Interpretacja:
+# Największa liczba wniosków dotyczy wkładów własnych w przedziale od 40 000 do 50 000, wskazując na koncentrację danych w tym zakresie.
+# Rozkład jest symetryczny, z niewielką liczbą wniosków dotyczących zarówno bardzo niskich (poniżej 20 000), jak i bardzo wysokich wkładów
+# (powyżej 60 000).
 
 
 # Cena towaru - histogram.
 ggplot(data, aes(x = cena_towaru)) +
-  geom_histogram(bins = 100, fill = "yellow", alpha = 0.7, color = "black") +
+  geom_histogram(bins = 30, fill = "yellow", alpha = 0.7, color = "black") +
   labs(title = "Rozkład cen towarów", x = "Cena towaru", y = "Liczba wniosków") +
   theme_minimal() +
-  scale_x_continuous(labels = scales::comma)
+  scale_x_continuous(labels = scales::comma, limits = c(0, 3000000), breaks = seq(0, 3000000, by = 500000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 3000), breaks = seq(0, 3000, by = 500))
+
+# Interpretacja:
+# Największa liczba wniosków dotyczy towarów o cenie poniżej 500 000, co jest zauważalne po wysokim słupku po lewej stronie,
+# gdy w tym samym czasie liczba wniosków znacząco maleje wraz ze wzrostem ceny towaru.
+# Rozkład jest prawostronnie skośny, a pojedyncze wnioski dotyczą towarów o bardzo wysokich cenach, przekraczających 2 000 000.
 
 # Roczna rata - histogram
 ggplot(data, aes(x = roczna_rata)) +
-  geom_histogram(bins = 100, fill = "lightblue", alpha = 0.7, color = "black") +
+  geom_histogram(bins = 30, fill = "lightblue", alpha = 0.7, color = "black") +
   labs(title = "Rozkład rocznej raty", x = "Roczna rata", y = "Liczba wniosków") +
   theme_minimal() +
-  scale_x_continuous(labels = scales::comma)
-
+  scale_x_continuous(labels = scales::comma, limits = c(0, 250000), breaks = seq(0, 250000, by = 50000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 10000), breaks = seq(0, 10000, by = 1000))
+  
+# Interpretacja:
+# Dominują nieskie wartości, gdzie większość wniosków dotyczy rat poniżej 50 000, co jest zauważalne po wysokim słupku po lewej stronie,
+# gdy w tym samym czasie liczba wniosków znacząco maleje wraz ze wzrostem wysokości raty.
+# Rozkład jest prawostronnie asymetryczny, przy czym tylko niewielka liczba wniosków dotyczy bardzo wysokich rat, przekraczających 150 000.
 
 # Komunikat
 cat("Porównanie histogramów danych liczbowych w grupach...\n")
@@ -105,10 +135,16 @@ cat("Porównanie histogramów danych liczbowych w grupach...\n")
 ggplot(data, aes(x = wnioskowana_kwota, fill = typ_umowy)) +
   geom_histogram(bins = 30, alpha = 0.7, position = "identity") + 
   labs(title = "Rozkład wnioskowanej kwoty w podziale na typ umowy",
-       x = "Wnioskowana kwota", y = "Liczba wniosków") +
+       x = "Wnioskowana kwota", y = "Liczba wniosków", fill = "Typ umowy") +
   theme_minimal() +
-  scale_x_continuous(labels = scales::comma) +
+  scale_x_continuous(labels = scales::comma, limits = c(0, 250000), breaks = seq(0, 250000, by = 50000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 5000), breaks = seq(0, 5000, by = 500))
   scale_fill_brewer(palette = "Set2")
+  
+# Interpretacja:
+# Największa liczba wniosków dotyczy kredytów gotówkowych, szczególnie w przedziale około 100 000, gdy w tym samym czasie
+# kredyty konsumenckie i odnawialne koncentrują się na niższych kwotach, poniżej 50 000.
+# Rozkład dla wszystkich typów umów jest asymetryczny, gdzie większość wniosków dotyczy niskich kwot oraz sporadycznie występują także wysokie wartości.
 
 # Cena towarów a kategoria portfela.
 ggplot(data, aes(x = cena_towaru, fill = kategoria_portfela)) +
@@ -116,8 +152,14 @@ ggplot(data, aes(x = cena_towaru, fill = kategoria_portfela)) +
   scale_fill_manual(values = colorRampPalette(brewer.pal(12, "Set3"))(27))+
   labs(title = "Rozkład cen towarów w podziale na kategorie portfela",
        x = "Cena towaru", y = "Liczba wniosków", fill = "Kategoria portfela") +
-  theme_minimal()
-  
+  theme_minimal() +
+  scale_x_continuous(labels = scales::comma, limits = c(0, 2500000), breaks = seq(0, 2500000, by = 500000)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 2000), breaks = seq(0, 5000, by = 500))
+
+# Interpretacja:
+# Większość wniosków dotyczy towarów o niskich cenach (poniżej 500 000) niezależnie od kategorii.
+# Rozkład jest prawostronnie asymetryczny, gdzie dominują niskie wartości, a wyższe ceny (szczególnie powyżej 1 000 000)
+# występują sporadycznie.
 
 # Komunikat
 cat("Zależności między poszczególnymi zmiennymi...\n")
